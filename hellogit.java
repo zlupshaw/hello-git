a365e1d618eabb5f28470924b8dcb0e77578bc7f
Zachary Upshaw <zlupshaw@gmail.com>
2017-03-09 17:36:29 -0300

    Hello git message

--- hellogit.java
+++ hellogit.java
@@ -1,6 +1,7 @@
-public class hellogit {
-public static void main(String[] args)
+public class hellogit 
 {
-System.out.println(Helo git)
-}
+	public static void main(String[] args)
+	{
+	System.out.println("Hello git");
+	}
 }
